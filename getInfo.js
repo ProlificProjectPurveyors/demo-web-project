80d1434121ee75597afd885c4747d07167307644
diegob1 <diegob1@cpp.edu>
2018-11-28 15:47:43 -0800

    Now works automatically with file name: clubInfo.txt

--- getInfo.js
+++ getInfo.js
@@ -14,28 +14,25 @@ var major = newRow.insertCell(4);
 var descrip = newRow.insertCell(5);
 // Append a text node to the cell
 
+
+/*$.get( "clubInfo.txt", function( data ) {
+  alert( "Data Loaded: " + data );
+});*/ //data == textFile.result
+
 var i = 1;
 var j = 1;
 var loopCounter = 0;
-var filesInput = document.getElementById("file");
-
-console.log(document.getElementById("file"));
-filesInput.addEventListener("change", function (event) {
-    var files = event.target.files;
-	
-    var file = files[0];
-    var reader = new FileReader();
-    reader.addEventListener("load", function (event) {
-        var textFile = event.target;
-
-		while(i+2< textFile.result.length)
+$.get( "clubInfo.txt", function( data ) {
+        //alert(textFile.result[1]+ textFile.result[2] + textFile.result[3]);
+		//roomNumber.appendChild(textFile.result[i]);
+		//while(reader.AtEndOfStream)
+		//{
+		var myLoc = data[i];
+		//alert(myLoc);
+		while(data[i+1] !== "\"")
 		{
-		var myLoc = textFile.result[i];
-
-		while(textFile.result[i+1] !== "\"")
-		{
-
-			myLoc = myLoc+textFile.result[i+1];
+			myLoc = myLoc+data[i+1];
+			//console.log(myLoc);
 			i++;
 		}
 		
@@ -43,11 +40,11 @@ filesInput.addEventListener("change", function (event) {
 		roomNumber.appendChild(myLoc2);
 		i = i+4;
 		//roomNumber= newRow.insertCell(j);
-		var theClubName = textFile.result[i];
-		while(textFile.result[i+1] !== "\"")
+		var theClubName = data[i];
+		while(data[i+1] !== "\"")
 		{
-			theClubName = theClubName+textFile.result[i+1];
-
+			theClubName = theClubName+data[i+1];
+			//console.log(theClubName);
 			i++;
 		}
 		
@@ -55,30 +52,23 @@ filesInput.addEventListener("change", function (event) {
 		clubName.appendChild(theClubName2);
 		
 		i = i+4;
-
+		//console.log("Stuff here is " + i);
 		//clubName = newRow.insertCell(j+1);
-		var theDate = textFile.result[i];
-		while(textFile.result[i+1] !== "\"")
+		var theDate = data[i];
+		while(data[i+1] !== "\"")
 		{
-			theDate = theDate+textFile.result[i+1];
-
+			theDate = theDate+data[i+1];
+			//console.log(theDate);
 			i++;
 		}
 		
 		var theDate2= document.createTextNode(theDate);
 		date.appendChild(theDate2);
 		//date = newRow.insertCell(j+2);
-		i = i+6;
-
-
-		newRow   = tableRef.insertRow(tableRef.rows.length);
-		roomNumber = newRow.insertCell(0);
-		clubName  = newRow.insertCell(1);
-		date = newRow.insertCell(2);
-		loopCounter++;
-		}
-
 		
-    });
-    reader.readAsText(file);
+		//console.log(textFile.result[i]+textFile.result[i+1]);
+		//console.log(i);
+		//console.log("End of file is " + textFile.result[45]);
+		
+		//}
 });
